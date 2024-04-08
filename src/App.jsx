0ec1e8e00b8fb30fe1dfdfd4876789b9e814e860
Aryama <aryama@Aryamas-MacBook-Air.local>
2024-04-09 00:29:42 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 import { Button as BaseButton, buttonClasses } from '@mui/base/Button';
 
 const divStyle = {
@@ -92,12 +93,23 @@ export default function App() {
   return (
   <center>
     <main>
-          <Box sx={{ flexGrow: 1 }}>
+      
+    <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar variant="dense">
-          <Typography variant="h6" color="inherit" component="div">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             QuickShare
           </Typography>
+          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
