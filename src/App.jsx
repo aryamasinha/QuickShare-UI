1c74d959a47d2b987ec5d76cac87b466d11fccd3
Aryama <aryama@Aryamas-MacBook-Air.local>
2024-04-09 00:34:57 +0530

    button app bar

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import ButtonAppBar from './Navbar';
 import { Button as BaseButton, buttonClasses } from '@mui/base/Button';
 
 const divStyle = {
@@ -93,26 +94,8 @@ export default function App() {
   return (
   <center>
     <main>
-      
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            QuickShare
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    
+    <ButtonAppBar></ButtonAppBar>
     <h2 style={divStyle}>Enter Code Snippet</h2>
       <center>
       <Textarea
